1654399b0bdb6953e10279007b856653088b409e
Jameyi <jameyijameyi@163.com>
2017-11-04 22:36:37 +0800

    Update readjson.py

--- readjson.py
+++ readjson.py
@@ -2,19 +2,20 @@ import re
 import urllib2
 import json
 
+urlstr = 'http://fundgz.1234567.com.cn/js/001186.js?rt=1463558676006'
+urlstr2 = 'http://fund.eastmoney.com/js/fundcode_search.js'
+ 
 def jsonp2json(jsonp_str):
 	try:
 		return re.search('^[^(]*?\((.*)\)[^)]*$',jsonp_str).group(1)
 	except:
 		raise ValueError('Invalid JSONP')
 
-
-html = urllib2.urlopen('http://fundgz.1234567.com.cn/js/001186.js?rt=1463558676006')
-
+html = urllib2.urlopen(urlstr)
 htmlr = html.read()
 hjson = jsonp2json(htmlr)
-
 #hjson = eval(hjson)
 
 hjson = json.loads(hjson)
-print(hjson)
+print(hjson['name'])
+print(hjson['fundcode'],hjson['name'],hjson['jzrq'],hjson['dwjz'],hjson['gsz'],hjson['gszzl'],hjson['gztime'])
