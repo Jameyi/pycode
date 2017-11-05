06aed59c83a1f482d4726710b08ace85dbb97aa3
Jameyi <jameyijameyi@163.com>
2017-11-06 01:32:49 +0800

    Update px_retrieving_yh.py

--- px_retrieving_yh.py
+++ px_retrieving_yh.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 
 import datetime
-import pandas.io.data as web
+import pandas_datareader.data as web
 
 if __name__ == "__main__":
-	spy = web.DataReader("SPY","yahoo",datetime.datetime(2007,1,1),datetime.datetime(2015,6,15))
+	spy = web.DataReader("SPY","yahoo",datetime.datetime(2017,1,1),datetime.datetime.today())
 	print(spy.tail())
