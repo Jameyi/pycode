653caaa6a5bbb8c3185914294f002c313fc92aa6
Jameyi <jameyijameyi@163.com>
2017-11-05 10:54:29 +0800

    Update readjson.py

--- readjson.py
+++ readjson.py
@@ -19,3 +19,35 @@ hjson = jsonp2json(htmlr)
 hjson = json.loads(hjson)
 print(hjson['name'])
 print(hjson['fundcode'],hjson['name'],hjson['jzrq'],hjson['dwjz'],hjson['gsz'],hjson['gszzl'],hjson['gztime'])
+
+
+############################################################################################################
+import re
+import urllib2
+import json
+from bs4 import BeautifulSoup as bs
+
+urlstr = 'http://fund.eastmoney.com/allfund.html'
+
+def jsonp2json(jsonp_str):
+	try:
+		return re.search('^[^(]*?\((.*)\)[^)]*$',jsonp_str).group(1)
+	except:
+		raise ValueError('Invalid JSONP')
+
+def url2json(urlstr):
+	html = urllib2.urlopen(urlstr)
+	htmlr = html.read()
+	hjson = jsonp2json(htmlr)
+	hjson = json.loads(hjson)
+	return hjson
+
+if __name__ == "__main__":
+	html = urllib2.urlopen(urlstr)
+	bsobj = bs(html)
+	divs = bsobj.findAll('div')
+	for div in divs:
+		for ass in div.children:
+			astr = str(ass.string)
+			print(type(astr))
+	
