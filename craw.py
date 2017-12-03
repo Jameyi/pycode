b89f709ba49e1e44934e2327c2c917ceb9f10a79
Jameyi <jameyijameyi@163.com>
2017-12-03 16:44:57 +0800

    Update craw.py

--- craw.py
+++ craw.py
@@ -22,9 +22,12 @@ def getLinks(pageUrl):
 
 from bs4 import BeautifulSoup as bs
 import urllib2
+import re
 
 urlstr = "http://fund.eastmoney.com/"
 fundcode = '161725'
+position_list = []
+uprise_list = []
 
 if __name__ == "__main__":
 	# 访问网页
@@ -35,11 +38,22 @@ if __name__ == "__main__":
 	# itml = bsobj.find("dl",{"class":"dataItem03"}).find("dd").find("span").get_text()
 	# print(itm)
 	# print(itml)
+	######################找出基金的十大股票持仓并保存到列表################
 	trs = bsobj.find("table",{"class":"ui-table-hover"}).findAll("tr")
 	for i in range(len(trs)-1):
-		print(trs[i+1].find('td').get_text())
+		position = trs[i+1].find('td').get_text()
+		position_list.append(position)
 	# ths = trs.findAll("th")
 	# print(ths[1].get_text())
+	######################找出历来涨幅并保存到列表#########################
+	trs = bsobj.find("li",{"id":"increaseAmount_stage"}).findAll("tr")
+	tds = trs[1].findAll("td")
+	for td in tds:
+		div = td.find("div",attrs={"class":re.compile("(Rdata)[a-z]*")})
+		if div != None:
+			uprise = div.get_text()
+			uprise_num = uprise.decode('unicode-escape').encode('utf-8').replace('%','0').replace('--','0')
+			uprise_list.append(float(uprise_num))
 		
  
 
