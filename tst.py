6a6837eae725eab04a7da2350c7298d4ad61173f
Jameyi <jameyijameyi@163.com>
2017-12-25 22:45:59 +0800

    Update tst.py

--- tst.py
+++ tst.py
@@ -5,3 +5,22 @@ rstr = '10'
 print(rstr,type(rstr))
 res = bin(int(rstr,10))
 print(res,type(res))
+
+mysql -u root -p jameyi
+
+CREATE DATABASE fund;
+USE fund
+
+CREATE TABLE fundinfo(
+id int NOT NULL AUTO_INCREMENT,
+fundcode varchar(6) NOT NULL,
+fundname varchar(255) NULL,
+fundtype varchar(6) NULL,
+listed_date datetime NULL,
+created_date datetime NOT NULL,
+last_update_date datetime NULL,
+accumulate_net_value decimal(6,4) NULL,
+unit_net_value decimal(6,4) NULL,
+stock_position_1_code varchar(6) NULL,
+PRIMARY KEY(id)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
